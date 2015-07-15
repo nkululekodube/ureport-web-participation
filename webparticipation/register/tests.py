0df1259152a2d8090d6546685f5c61a5aabc0d34
Steven Liu <steve@stevenliu.com>
2015-07-15 16:12:40 +0300

    [steve & jackie] - registration test

--- webparticipation/register/tests.py
+++ webparticipation/register/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from register.models import Registration
 
-# Create your tests here.
+class TestRegistration(TestCase):
+
+    def test_get_registration_is_ok(self):
+        response = self.client.get('/register/')
+        self.assertEquals(200, response.status_code)
+        self.assertTemplateUsed(response, 'register.html')
