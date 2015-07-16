63b26e3e53741cc8e5fecde74f6aeb2e7a8045f2
Steven Liu <steve@stevenliu.com>
2015-07-16 10:30:26 +0300

    [steve & jackie] - change project structure

--- webparticipation/register/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from register.models import Registration
-
-class TestRegistration(TestCase):
-
-    def test_get_registration_is_ok(self):
-        response = self.client.get('/register/')
-        self.assertEquals(200, response.status_code)
-        self.assertTemplateUsed(response, 'register.html')
