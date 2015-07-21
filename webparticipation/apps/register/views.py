5234de7394eb78a7bdcf58b0bad4f06bc8213330
katmutua <jkm.mutua@gmail.com>
2015-07-21 10:37:14 +0300

    [jackie] - #22 - fix indents

--- webparticipation/apps/register/views.py
+++ webparticipation/apps/register/views.py
@@ -7,27 +7,30 @@ import requests
 
 messages = []
 
+
 def register(request):
     response = HttpResponse()
     user = get_user(request)
     context = {}
 
     if request.method == 'GET':
-        requests.post(settings.RAPIDPRO_URL, data={ 'from': user, 'text': 'webregister' })
+        requests.post(settings.RAPIDPRO_URL, data={'from': user, 'text': 'webregister'})
         user_messages = get_messages_for_user(user)
         response = render(request, 'register.html', {'messages': user_messages})
         response.set_cookie(key='userid', value=user)
 
     if request.method == 'POST':
-        requests.post(settings.RAPIDPRO_URL, data={ 'from': user, 'text': request.POST['send'] })
+        requests.post(settings.RAPIDPRO_URL, data={'from': user, 'text': request.POST['send']})
         user_messages = get_messages_for_user(user)
         response = render(request, 'register.html', {'messages': user_messages})
 
     print request.method + ' messages', messages
     return response
 
+
 def get_user(request):
     return request.COOKIES.get('userid') or 'user' + str(randint(100000000, 999999999))
+
 
 def get_messages_for_user(user_id):
     global messages
@@ -35,14 +38,17 @@ def get_messages_for_user(user_id):
         sleep(.5)
     return filter_messages(user_id)
 
+
 def filter_messages(user_id):
     global messages
     filtered_messages = filter(lambda msg: msg['msg_to'] == user_id, messages)
     messages = filter(lambda msg: msg['msg_to'] != user_id, messages)
     return filtered_messages
 
+
 def rapidpro_dispatcher_callback(sender, **kwargs):
-    print 'dispatcher', sender, kwargs['param_id'], kwargs['param_channel'], kwargs['param_from'], kwargs['param_to'], kwargs['param_text']
+    print 'dispatcher', sender, kwargs['param_id'], kwargs['param_channel'], kwargs['param_from'], kwargs['param_to'], \
+        kwargs['param_text']
     global messages
     messages.append({
         'msg_id': kwargs['param_id'],
@@ -52,5 +58,6 @@ def rapidpro_dispatcher_callback(sender, **kwargs):
         'msg_text': kwargs['param_text']
     })
     print 'messages', messages
+
 
 settings.RAPIDPRO_DISPATCHER.connect(rapidpro_dispatcher_callback)
