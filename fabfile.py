c29b2f7bcd71f77adffeec5bdc62f863abe5b0d7
Jackie k. Mutua <jkm.mutua@gmail.com>
2015-07-27 11:39:35 +0300

    [jackie & benneth]  - #tech-tasks - update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from __future__ import with_statement
 from fabric.api import *
 
-env.key_filename = ['webpro-key.pem']
+env.key_filename = ['wep-pro-key.pem']
 env.hosts = ["ubuntu@ec2-52-18-73-186.eu-west-1.compute.amazonaws.com"]
 
 
@@ -11,8 +11,12 @@ def deploy():
         run("git pull")
 
     with cd("www/ureport-web-participation"):
-        run("source env/bin/activate && pip install -r reqs/dev.txt")
+        run("source env/bin/activate && pip install -r reqs/common.txt")
 
+    with cd("www/ureport-web-participation"):
+        run("python manage.py makemigrations")
+        run("pyhton manage.py migrate")
+        
     with cd('www/ureport-web-participation/'):
         sudo("supervisorctl restart uwsgi-builder")
         sudo("supervisorctl restart builder_celery")
