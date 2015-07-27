31455445d6840e4644e66f19aba9450f855c1755
Musiime Beneth <musiime.beneth@gmail.com>
2015-07-27 10:40:43 +0300

    #22 [Beneth] Removed the testing sample code and refactored the feature test for global user verifies email address

--- tests/environment.py
+++ tests/environment.py
@@ -1,11 +1,7 @@
-from behave import *
 from splinter.browser import Browser
-from django.core import management
-from selenium import webdriver
 
 def before_all(context):
-    context.browser = Browser('firefox')
-    # driver = webdriver.Firefox()
+    context.browser = Browser('chrome')
 
 def after_all(context):
     context.browser.quit()
--- tests/steps/email_step.py
+++ tests/steps/email_step.py
@@ -1,51 +1,71 @@
-import os
 from behave import *
-# from splinter import Browser
-from splinter import driver
 import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-email = 'mben03@gmail.com';
 
-@given(u'I am a new user')
+email = 'mben03@gmail.com'
+wrong_code = '3457823232'
+wrong_email = 'mben03&gmail.com'
+
+def go_to_u_report(browser):
+    browser.visit('http://localhost:8000/register')
+    browser.find_by_id('djHideToolBarButton').click()
+
+
+@given(u'I am a new user  with a wrong email format')
+def step_impl(context):
+    go_to_u_report(context.browser)
+    # pass
+
+@when(u'I browse to u-report')
 def step_impl(context):
     pass
 
-
-@when(u'I browse to u-report website')
+@when(u'I enter a wrong format for an email address')
 def step_impl(context):
-    context.browser.visit('http://localhost:8000/register')
-
-@when(u'I fill in critical registration details')
+    time.sleep(1)
+    context.browser.fill('send', wrong_email)
+@when(u'I click submit')
 def step_impl(context):
-    # wait_page_load(context.browser.find_by_id('btnSend'))
+    context.browser.find_by_css('.wp-send.btn').first.click()
+    time.sleep(1)
+
+@then(u'I should see a message "that does not seem to be a valid email address."')
+def step_impl(context):
+    time.sleep(1)
+    assert context.browser.is_text_present("that does not seem to be a valid email address."), 'Invalid email'
+
+@given(u'I am a new user with a correct email format')
+def step_impl(context):
+    pass
+
+@when(u'I fill in my email address')
+def step_impl(context):
     context.browser.fill('send', email)
+    time.sleep(1)
 
-@when(u'I click submit critical registration details')
+@then(u'I shall see "my email" and a notification "Thanks! Just to confirm that you are actually you".')
 def step_impl(context):
-    send = context.browser.find_by_id('btnSend')
-    send.click()
-    time.sleep(0)
-
-@then(u'I shall be a registered u-reporter.')
-def step_impl(context):
-
-    # attributes = context.browser.find_by_css("ul.messages")
-    # for attribute in attributes:
-    #     if attribute.find_by_tag('li').first.value == 'mben03@gmail.com':
-    #         print ("Email registered")
-    #     else:
-    #         print ("Email not registered")
-
     assert context.browser.is_text_present(email), 'Email not registered..'
+    time.sleep(1)
 
 
-    if context.browser.is_text_present(email):
-        print ("Email registered")
-    else:
-        print ("Email not registered")
-# send |btnSend
+@given(u'I am a new user who has submitted their email address')
+def step_impl(context):
+    pass
+@when(u'I misspell the verification code')
+def step_impl(context):
+    context.browser.fill('send', wrong_code)
 
+@then(u'Then I should see a message "I\'m sorry, that\'s an invalid code."')
+def step_impl(context):
+    time.sleep(1)
+    assert context.browser.is_text_present("I'm sorry, that's an invalid code."), 'Invalid code.1'
+
+@given(u'I am already registered')
+def step_impl(context):
+    pass
+
+@then(u'I shall see a notification "Email already registered".')
+def step_impl(context):
+    # assert context.browser.is_text_present('Email already registered'), 'Sorry, email already registered..'
+    pass
