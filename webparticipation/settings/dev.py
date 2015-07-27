a911fa7a9c4b5630c24eb34676ba4855f6e5fd72
Jackie k. Mutua <jkm.mutua@gmail.com>
2015-07-27 15:15:16 +0300

    [jackie & benneth]  - #tech-tasks - update dev settings

--- webparticipation/settings/dev.py
+++ webparticipation/settings/dev.py
@@ -13,7 +13,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'webparticipation',
-        'USER': 'admin',
+        'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': os.environ.get('DATABASE_PORT'),
