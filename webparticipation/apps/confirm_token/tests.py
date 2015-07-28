c47aa29cf95de71d3a33228773e0797e4b2d2f95
Steven Liu <steve@stevenliu.com>
2015-07-28 17:14:03 +0300

    [steve] - send token unit tests

--- webparticipation/apps/confirm_token/tests.py
+++ webparticipation/apps/confirm_token/tests.py
@@ -3,19 +3,13 @@ from django.http import HttpResponse, QueryDict
 from webparticipation.apps.ureporter.models import Ureporter
 import requests
 from . import views
+from django.test.client import RequestFactory
+
 
 class TestConfirmToken(TestCase):
 
     def setUp(self):
         self.user = Ureporter.objects.create(uuid='f3a12ae7-4f05-4fce-8135-bc51a9522116')
 
-
     def test_confirm_token_with_good_code(self):
         pass
-        # post_params = QueryDict('phone=f3a12ae74f054fce8135bc51a9522116&text=' + str(self.user.token))
-        # self.params = {
-        #     'POST': post_params
-        # }
-        # self.response = self.client.post('/confirm-token', self.params)
-        # self.assertEqual(200, self.response.status_code)
-        # self.assertEqual({'status': 200}, self.response)
--- webparticipation/apps/send_token/tests.py
+++ webparticipation/apps/send_token/tests.py
@@ -1,9 +1,26 @@
 from django.test import TestCase
 from webparticipation.apps.ureporter.models import Ureporter
+from django.test.client import RequestFactory
+from webparticipation.apps.ureporter.models import Ureporter
+from views import get_uuid, send_token
+import json
 
 
 class TestSendToken(TestCase):
 
     def setUp(self):
+        self.factory = RequestFactory()
         self.uuid = 'f3a12ae7-4f05-4fce-8135-bc51a9522116'
+        self.undashified_uuid = 'f3a12ae74f054fce8135bc51a9522116'
         self.user = Ureporter.objects.create(uuid=self.uuid)
+
+    def test_send_token(self):
+        request = self.factory.post('/send-token', {'phone': self.undashified_uuid, 'text': 'an@ok.address'})
+        response = send_token(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content)['send_token'], 'send')
+
+    def test_get_uuid(self):
+        request = self.factory.post('/send-token', {'phone': self.undashified_uuid, 'text': 'whatever'})
+        uuid = get_uuid(request)
+        self.assertEqual(self.uuid, uuid)
