09604594d5ef209f3d25dbf8c54993c07003af6f
Steven Liu <steve@stevenliu.com>
2015-07-29 22:21:28 +0300

    [steve] - #ft - remove server startup

--- tests/environment.py
+++ tests/environment.py
@@ -4,15 +4,13 @@ import time
 
 
 def before_all(context):
-    context.server = subprocess.Popen(['python', 'manage.py', 'runserver', '8200'])
-    print (context.server)
+    # context.server = subprocess.Popen(['python', 'manage.py', 'runserver', '8200'])
     context.browser = Browser('chrome')
-    time.sleep(5)
+    # time.sleep(5)
 
 
 def after_all(context):
     context.browser.quit()
     context.browser = None
-    subprocess.Popen('lsof -t -i:8200 | xargs kill', shell=True)
-    context.server = None
-    print (context.server)
+    # subprocess.Popen('lsof -t -i:8200 | xargs kill', shell=True)
+    # context.server = None
