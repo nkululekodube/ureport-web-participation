48e103e4efe88ce98cb080f40a94e99e9e772e2f
Steven Liu <steve@stevenliu.com>
2015-07-29 15:04:51 +0300

    [steve & beneth] - #34 - get server up before ft & kill after

--- tests/environment.py
+++ tests/environment.py
@@ -1,8 +1,18 @@
 from splinter.browser import Browser
+import subprocess
+import time
+
 
 def before_all(context):
+    context.server = subprocess.Popen(['python', 'manage.py', 'runserver', '8200'])
+    print (context.server)
     context.browser = Browser('chrome')
+    time.sleep(5)
+
 
 def after_all(context):
     context.browser.quit()
     context.browser = None
+    subprocess.Popen('lsof -t -i:8200 | xargs kill', shell=True)
+    context.server = None
+    print (context.server)
