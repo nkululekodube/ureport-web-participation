8789faf55db9ab83a19278a1a64b17ae1b83a6c7
katmutua <jkm.mutua@gmail.com>
2015-08-03 19:36:26 +0300

    [jackie] - #20 - update send_forgot_password_email task

--- webparticipation/apps/ureport_auth/tasks.py
+++ webparticipation/apps/ureport_auth/tasks.py
@@ -11,20 +11,18 @@ from webparticipation.apps.ureporter.models import Ureporter
 def send_forgot_password_email(email):
     try:
         user = User.objects.get(email=email)
-
         if user:
             expiry = timezone.now() + timezone.timedelta(days=settings.PASSWORD_RESET_EXPIRY_DAYS)
             password_reset = PasswordReset.build(expiry, user)
             password_reset.save()
-
             uuid = Ureporter.objects.get(user_id=password_reset.user_id).uuid
 
             link = reset_password_url("/password-reset/%s" % uuid)
-            in_text = "<p>Please click this link to change " \
+            html_text = "<p>Please click this link to change " \
                       "your password on Ureport.in <a href='" + link + "'> Email reset link</a></p>"
             subject = 'Hi from ureport.in'
 
-            body = in_text + '<p>-----Thanks</p>'
+            body = html_text + '<p>-----Thanks</p>'
             signature = '\nureport team'
             recipients = [email]
 
