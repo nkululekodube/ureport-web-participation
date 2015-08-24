d105fc696e90588e9d2c43301a62c68bbbd4e880
katmutua <jkm.mutua@gmail.com>
2015-08-24 09:43:22 +0300

    [jackie] - #51 - clean up prod settiings

--- webparticipation/settings/prod.py
+++ webparticipation/settings/prod.py
@@ -1,12 +1,6 @@
 """Production settings and globals."""
 
-
 from os import environ
-
-from memcacheify import memcacheify
-from postgresify import postgresify
-from S3 import CallingFormat
-
 from common import *
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -17,64 +11,18 @@ EMAIL_HOST_PASSWORD = environ.get('EMAIL_HOST_PASSWORD', '')
 
 EMAIL_HOST_USER = environ.get('EMAIL_HOST_USER', 'your_email@example.com')
 
-EMAIL_PORT = environ.get('EMAIL_PORT', 587)
-
-EMAIL_SUBJECT_PREFIX = '[%s] ' % SITE_NAME
+EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-SERVER_EMAIL = EMAIL_HOST_USER
-
-DATABASES = postgresify()
-
-CACHES = memcacheify()
-
 BROKER_TRANSPORT = 'amqplib'
 
-# Set this number to the amount of allowed concurrent connections on your AMQP
-# provider, divided by the amount of active workers you have.
 BROKER_POOL_LIMIT = 3
 
 BROKER_CONNECTION_MAX_RETRIES = 0
 
 BROKER_URL = environ.get('RABBITMQ_URL') or environ.get('CLOUDAMQP_URL')
 
-CELERY_RESULT_BACKEND = 'amqp'
-
-INSTALLED_APPS += (
-    'storages',
-)
-
-STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
-
-AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID', '')
-AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY', '')
-AWS_STORAGE_BUCKET_NAME = environ.get('AWS_STORAGE_BUCKET_NAME', '')
-AWS_AUTO_CREATE_BUCKET = True
-AWS_QUERYSTRING_AUTH = False
-
-# AWS cache settings:
-AWS_EXPIRY = 60 * 60 * 24 * 7
-AWS_HEADERS = {
-    'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIRY,
-        AWS_EXPIRY)
-}
-
-STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
-
-COMPRESS_OFFLINE = True
-
-COMPRESS_STORAGE = DEFAULT_FILE_STORAGE
-
-COMPRESS_CSS_FILTERS += [
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-
-COMPRESS_JS_FILTERS += [
-    'compressor.filters.jsmin.JSMinFilter',
-]
 
 SECRET_KEY = environ.get('SECRET_KEY', SECRET_KEY)
 
