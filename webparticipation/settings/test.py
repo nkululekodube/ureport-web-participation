987ce8701ccd6881690fff077797050275dfa823
Musiime Beneth <musiime.beneth@gmail.com>
2015-08-27 13:48:14 +0300

    [Beneth] - added testing settings

---
+++ webparticipation/settings/test.py
@@ -0,0 +1,21 @@
+"""Development settings and globals."""
+
+from common import *
+
+DEBUG = True
+
+TEMPLATE_DEBUG = DEBUG
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+
+CELERY_ALWAYS_EAGER = True
+
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+
+INTERNAL_IPS = ('127.0.0.1',)
