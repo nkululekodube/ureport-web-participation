5d01f1ac468dbbb9e443dcc226814c1b5cbab2a0
Musiime Beneth <musiime.beneth@gmail.com>
2015-09-14 17:32:55 +0530

    [#53] merged reset_step to forgot_password_step

--- features/forgotpassword.feature
+++ features/forgotpassword.feature
@@ -1,8 +1,8 @@
 #### #22: A global user verifies email during registration process
 ##### User Login
-Feature: A registered user should be able to change my password
+Feature: A registered user has forgotten the password
 
-  Scenario: Resetting password for web-pro with a registered email
+  Scenario: User has forgotten the password for web-pro but has a registered email
 
     Given I am a registered user
     When I visit the login page
@@ -12,7 +12,7 @@ Feature: A registered user should be able to change my password
     And I shall see a notification 'We have sent an email ...'
 
 
-  Scenario: Resetting password for web-pro with non registered email
+  Scenario: User has forgotten the password for web-pro but submits a non-registered email
 
     Given I am a registered user
     When I visit the login page
@@ -20,3 +20,22 @@ Feature: A registered user should be able to change my password
     Then I see the page requesting me for an email address
     And I shall input a wrong email address
     And I shall see a notification 'There is no registered user ... '
+
+  Scenario: Resetting password for web-pro with a matching passwords
+    Given I am a registered user
+    When I visit the login page
+    And I click Forgot-password link
+    And I see the page requesting me for an email address
+    And I shall input my email address
+    And I shall see a notification 'We have sent an email ...'
+    And I go to reset password page
+    Then I submit my new password
+    And I see a notification of password changed
+    And I shall login with the new password
+
+  Scenario: Resetting password for web-pro with  passwords that do not match
+    Given I am a registered user
+    When I change visit reset password page
+    And I submit my un matching passwords
+    Then I see a notification of passwords do not match
+    And I shall login with the old password
--- features/steps/forgotpassword_step.py
+++ features/steps/forgotpassword_step.py
@@ -1,9 +1,12 @@
 from behave import *
 import time
-import os
-import page_actions
+import uuid
+from django.utils import timezone
 from django.contrib.auth.models import User
-from webparticipation.apps.ureporter.models import Ureporter
+from webparticipation.apps.ureport_auth.models import PasswordReset
+
+username = 'user999999999'
+expiry = timezone.now() + timezone.timedelta(days=1)
 
 @when(u'I click Forgot-password link')
 def step_impl(context):
@@ -18,14 +21,11 @@ def step_impl(context):
 
 @then(u'I shall input my email address')
 def step_impl(context):
-    # usr = User.objects.create(username=username1, email=email1, password=password)
-    # Ureporter.objects.create(user=usr)
     time.sleep(2)
     context.browser.fill('email', 'mben03@gmail.com')
     context.browser.find_by_css('.wp-send.btn').click()
     time.sleep(1)
-    print(User.objects.get(email='mben03@gmail.com').__dict__)
-
+    print(User.objects.get(email='mben03@gmail.com').__dict__, "Then User input \n")
 
 @then(u'I shall see a notification \'We have sent an email ...\'')
 def step_impl(context):
@@ -38,8 +38,94 @@ def step_impl(context):
     context.browser.fill('email', 'test@gmail.com')
     context.browser.find_by_css('.wp-send.btn').click()
 
-
 @then(u'I shall see a notification \'There is no registered user ... \'')
 def step_impl(context):
     time.sleep(2)
     assert context.browser.is_text_present('There is no registered user'), 'No registered user notification not found'
+
+@when(u'I see the page requesting me for an email address')
+def step_impl(context):
+    # pass
+    time.sleep(2)
+    assert context.browser.find_by_name('email'), 'Email field not found'
+
+@when(u'I shall input my email address')
+def step_impl(context):
+    pass
+    time.sleep(2)
+    context.browser.fill('email', 'mben03@gmail.com')
+    context.browser.find_by_css('.wp-send.btn').click()
+    time.sleep(1)
+    user = User.objects.get(username=username)
+    print(user.__dict__, "User \n")
+
+@when(u'I shall see a notification \'We have sent an email ...\'')
+def step_impl(context):
+    # pass
+    time.sleep(2)
+    assert context.browser.is_text_present('We have sent an email to mben03@gmail.com'), 'Email notification not found'
+
+@when(u'I go to reset password page')
+def step_impl(context):
+    time.sleep(2)
+    user = User.objects.get(username=username)
+    PasswordReset.objects.create(expiry=expiry, user=user, token=uuid.uuid4().hex)
+    password_reset = PasswordReset.objects.all()[0]
+    url = context.base_url + '/password-reset/' + str(password_reset.token)
+    print(password_reset.token, "Reset Password - token \n")
+    print(url, "URL \n")
+    print(password_reset.__dict__, "Reset Password \n")
+    context.browser.visit(url)
+    time.sleep(2)
+
+@then(u'I submit my new password')
+def step_impl(context):
+    time.sleep(2)
+    context.browser.fill('password', 'Password1')
+    context.browser.fill('confirm_password', 'Password1')
+    time.sleep(2)
+    context.browser.find_by_css('.wp-send.btn').click()
+
+@then(u'I see a notification of password changed')
+def step_impl(context):
+    pass
+
+@then(u'I shall login with the new password')
+def step_impl(context):
+    time.sleep(1)
+    context.browser.fill('email', 'mben03@gmail.com')
+    context.browser.fill('password', 'Password1')
+    time.sleep(2)
+    context.browser.find_by_css('.wp-send.btn').click()
+    time.sleep(5)
+    assert context.browser.find_link_by_href('/logout/'), 'Logout link not found'
+    context.browser.find_link_by_href('/logout/').click()
+
+@when(u'I change visit reset password page')
+def step_impl(context):
+    time.sleep(2)
+
+@when(u'I submit my un matching passwords')
+def step_impl(context):
+    time.sleep(1)
+    print(context.browser.url)
+    context.browser.fill('password', 'Password1')
+    context.browser.fill('confirm_password', 'Password2')
+    time.sleep(2)
+    context.browser.find_by_css('.wp-send.btn').click()
+
+@then(u'I see a notification of passwords do not match')
+def step_impl(context):
+    pass
+
+@then(u'I shall login with the old password')
+def step_impl(context):
+    context.browser.visit(context.base_url + 'login/')
+    time.sleep(1)
+    context.browser.fill('email', 'mben03@gmail.com')
+    context.browser.fill('password', 'pass')
+    time.sleep(2)
+    context.browser.find_by_css('.wp-send.btn').click()
+    time.sleep(5)
+    assert context.browser.find_link_by_href('/logout/'), 'Logout link not found'
+    context.browser.find_link_by_href('/logout/').click()
