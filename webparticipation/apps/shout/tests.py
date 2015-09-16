c896f85ebc933a676307f88e4e94d9e83809a545
James Muranga <jmured@gmail.com>
2015-09-16 12:54:24 +0300

    [MJ] #30 making sure that the message actual gets sent to the user

--- webparticipation/apps/shout/tests.py
+++ webparticipation/apps/shout/tests.py
@@ -1,7 +1,15 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from django.test import TestCase
 from django_webtest import WebTest
+from mock import patch
+import responses
+
+from webparticipation.apps.shout.views import user_in_flow
+
+from webparticipation.apps.ureporter.models import Ureporter
 
 
 class ShoutViewTestCase(WebTest):
@@ -12,18 +20,110 @@ class ShoutViewTestCase(WebTest):
 
     def setUp(self):
         self.user = User.objects.create(username="mainuser", email="me@email.com")
+        self.ureporter = Ureporter.objects.create(uuid="uuid", user=self.user, urn_tel="thecontact")
 
-    def test_requires_user_to_be_logged_in(self):
+    @patch('webparticipation.apps.shout.views.user_in_flow')
+    def test_requires_user_to_be_logged_in(self, mock_method):
+        mock_method.return_value = False, ""
         response = self.app.get(self.get_url())
         self.assertRedirects(response, 'http://testserver/login/?next=/shout')
 
-    def test_correct_template_is_loaded(self):
+    @patch('webparticipation.apps.shout.views.user_in_flow')
+    def test_correct_template_is_loaded(self, mock_method):
+        mock_method.return_value = False, ""
         response = self.app.get(self.get_url(), user=self.user)
         self.assertTemplateUsed(response, 'shout.html')
 
-    def test_should_require_message(self):
+    @patch('webparticipation.apps.shout.views.user_in_flow')
+    def test_should_require_message(self, mock_method):
+        mock_method.return_value = False, ""
         response = self.app.get(self.get_url(), user=self.user)
         form = response.form
         form['message'] = ''
         form_response = form.submit()
         self.assertEqual(form_response.context['form'].errors['message'][0], 'This field is required.')
+
+    @patch('webparticipation.apps.shout.views.send_message_to_rapidpro')
+    @patch('webparticipation.apps.shout.views.user_in_flow')
+    def test_should_send_message_to_ureport_if_valid(self, mock_method, mock_send_to_rapidpro):
+        mock_method.return_value = False, ""
+        response = self.app.get(self.get_url(), user=self.user)
+        form = response.form
+        form['message'] = 'the world works'
+        form.submit()
+        mock_send_to_rapidpro.assert_called_with({'text': 'the world works', 'from': 'thecontact'})
+
+    @patch('webparticipation.apps.shout.views.user_in_flow')
+    def test_should_show_message_if_user_in_flow(self, mock_method):
+        mock_method.return_value = True, "the poll flow uuid"
+        response = self.app.get(self.get_url(), user=self.user)
+        self.assertTemplateUsed(response, 'user_in_flow.html')
+
+
+class TestUserInFlow(TestCase):
+    username = "the wonder user"
+    uuid = "the user man"
+
+    def setUp(self):
+        self.user = User.objects.create_user(username=self.username)
+        self.ureporter = Ureporter.objects.create(uuid=self.uuid, user=self.user)
+        self.ureporter.save()
+
+    @responses.activate
+    def test_user_not_in_flow_if_all_runs_completed(self):
+        url = "%s/runs.json" % settings.RAPIDPRO_API_PATH
+        responses.add(responses.GET, url,
+                      body="""
+                      {
+  "count": 1,
+  "next": null,
+  "previous": null,
+  "results": [
+    {
+      "flow_uuid": "05c66a1e-ff50-4b46-8739-8e7efeb699b0",
+      "flow": 5,
+      "run": 48,
+      "completed": true
+    },
+    {
+      "flow_uuid": "05c66a1e-ff50-4b46-8e7efeb699b0",
+      "flow": 6,
+      "run": 48,
+      "completed": true
+    }
+  ]
+}
+                      """,
+                      status=200,
+                      content_type='application/json')
+        self.assertEqual(False, user_in_flow(self.user)[0])
+
+    @responses.activate
+    def test_user_in_flow_if_any_runs_incomplete(self):
+        url = "%s/runs.json" % settings.RAPIDPRO_API_PATH
+        responses.add(responses.GET, url,
+                      body="""
+                      {
+  "count": 1,
+  "next": null,
+  "previous": null,
+  "results": [
+    {
+      "flow_uuid": "the-test-flow-uid",
+      "flow": 5,
+      "run": 48,
+      "completed": false
+    },
+    {
+      "flow_uuid": "05c66a1e-ff50-4b46-8e7efeb699b0",
+      "flow": 6,
+      "run": 48,
+      "completed": true
+    }
+  ]
+}
+                      """,
+                      status=200,
+                      content_type='application/json')
+        self.assertEqual(True, user_in_flow(self.user)[0])
+        self.assertEqual('the-test-flow-uid', user_in_flow(self.user)[1])
--- webparticipation/apps/shout/views.py
+++ webparticipation/apps/shout/views.py
@@ -1,8 +1,16 @@
 from braces.views import LoginRequiredMixin
 from django import forms
+from django.conf import settings
+from django.contrib import messages
 from django.core.exceptions import ValidationError
 from django.forms import Form
+from django.http import HttpResponseRedirect
 from django.views.generic import FormView
+import requests
+
+from webparticipation.apps.rapidpro_receptor.views import send_message_to_rapidpro
+
+from webparticipation.apps.ureporter.models import Ureporter
 
 
 def validate_not_spaces(value):
@@ -11,11 +19,43 @@ def validate_not_spaces(value):
 
 
 class ShoutForm(Form):
-    message = forms.CharField(required=True, widget=forms.Textarea(attrs={'cols': 30, 'rows': 3}),
-                              validators=[validate_not_spaces])
+    message = forms.CharField(required=True, validators=[validate_not_spaces])
+
+
+def reduce_run(accumulator, item):
+    if not item.get('completed'):
+        accumulator[0] = True
+        accumulator[1] = item.get('flow_uuid', '')
+    return accumulator
+
+
+def user_in_flow(user):
+    reporter = Ureporter.objects.get(user=user)
+    params = {'contact': reporter.uuid}
+    headers = {'Authorization': 'Token %s' % settings.RAPIDPRO_API_TOKEN}
+    url = '%s/runs.json' % settings.RAPIDPRO_API_PATH
+    data = requests.get(url, params=params, headers=headers).json()
+    return reduce(reduce_run, data['results'], [False, ''])
 
 
 class ShoutView(LoginRequiredMixin, FormView):
     template_name = "shout.html"
     form_class = ShoutForm
     success_url = "/shout"
+
+    def get(self, request, *args, **kwargs):
+        in_flow, flow_id = user_in_flow(request.user)
+        if not in_flow:
+            form = self.get_form()
+            return self.render_to_response(self.get_context_data(form=form))
+        else:
+            self.template_name = "user_in_flow.html"
+            messages.info(self.request, 'Please complete the poll before you can send us a message')
+            return self.render_to_response(self.get_context_data())
+
+    def form_valid(self, form):
+        reporter = Ureporter.objects.get(user=self.request.user)
+        data = {'text': form.cleaned_data['message'], 'from': reporter.urn_tel}
+        send_message_to_rapidpro(data)
+        messages.info(self.request, 'Thank you change maker for sending ureport this message')
+        return HttpResponseRedirect(self.get_success_url())
