c58428a2ba38e19019b928437ad3111d3f17c1ea
Steven Liu <steve@stevenliu.com>
2015-09-22 11:08:28 +0300

    [steve] - #navigation - add top menu links

--- webparticipation/apps/home/views.py
+++ webparticipation/apps/home/views.py
@@ -1,14 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.conf import settings
 
-from webparticipation.apps.ureporter.models import Ureporter
 from webparticipation.apps.latest_poll.decorators import show_untaken_latest_poll_message
 
 
 @show_untaken_latest_poll_message
 def home(request):
-    uuid = None
-    if Ureporter.objects.filter(user__username=request.user).exists():
-        ureporter = Ureporter.objects.get(user__username=request.user)
-        if ureporter:
-            uuid = ureporter.uuid
-    return render(request, 'index.html', {'uuid': uuid})
+    return HttpResponseRedirect(settings.UREPORT_ROOT)
