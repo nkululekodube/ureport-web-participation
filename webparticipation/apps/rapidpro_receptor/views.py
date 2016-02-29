1fadc0d480be55c6ededf58cd6a9d7803bcecae9
nomadop <nomadop@gmail.com>
2016-02-29 15:38:12 +0800

    Add more space above and below page content. Adjust layout of register page. Add 2s sleep after receive message from rapidpro.

--- webparticipation/apps/rapidpro_receptor/views.py
+++ webparticipation/apps/rapidpro_receptor/views.py
@@ -43,6 +43,7 @@ def get_messages_for_user(username):
             return [_('Sorry, something went wrong.')], False
         else:
             sleep(.5)
+    sleep(2)
     sorted_message_ids = get_sorted_message_ids(username)
     messages_from_rapidpro = get_messages_from_rapidpro_api(sorted_message_ids)
     full_text_messages = [msg['results'][0]['text'] for msg in messages_from_rapidpro]
